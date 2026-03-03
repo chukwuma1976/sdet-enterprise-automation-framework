fe0aee68cbcf6159c69258009c01595ea7aa36dd
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2026-03-02 23:32:12 -0600

    Added Authentication and Authorization Tests

--- src/test/java/com/chukwuma/sdet/models/User.java
+++ src/test/java/com/chukwuma/sdet/models/User.java
@@ -1,6 +1,26 @@
 package com.chukwuma.sdet.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
-    public String username;
-    public String password;
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
