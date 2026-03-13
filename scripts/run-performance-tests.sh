2aaeb77009bf15459cd16d961bb41e278c2a90f4
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2026-03-13 13:02:10 -0500

    Spike test to be informational, failures will only be documented

--- scripts/run-performance-tests.sh
+++ scripts/run-performance-tests.sh
@@ -6,5 +6,5 @@ k6 run performance-tests/k6/smoke/users-smoke.js
 echo "Running k6 load test..."
 k6 run performance-tests/k6/load/users-load.js
 
-echo "Running k6 spike test..."
-k6 run performance-tests/k6/spike/users-spike.js
+echo "Running k6 spike test expect failures under heavy load..."
+k6 run performance-tests/k6/spike/users-spike.js || true
