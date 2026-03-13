6e1ad7d5f4f73e7581208cf95cca325d82983954
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2026-03-13 17:14:48 -0500

    Enabled performance test reporting

--- performance-tests/k6/spike/users-spike.js
+++ performance-tests/k6/spike/users-spike.js
@@ -1,6 +1,13 @@
 import { SPIKE_OPTIONS, TEST_URL } from '../config/performance-config.js';
 import { getUsers } from '../utils/api-helper.js';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = SPIKE_OPTIONS;
 
 export default () => getUsers(TEST_URL);
+
+export function handleSummary(data) {
+    return {
+        "performance-tests/results/spike-report.html": htmlReport(data),
+    };
+}
--- performance-tests/k6/utils/api-helper.js
+++ performance-tests/k6/utils/api-helper.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { check } from 'k6';
+import { sleep } from 'k6';
 
 export function getUsers(url) {
 
@@ -8,6 +9,8 @@ export function getUsers(url) {
     check(res, {
         'status is 200': (r) => r.status === 200
     });
+
+    sleep(Math.random() * 3); //Simulate user think time
 
     return res;
 }
